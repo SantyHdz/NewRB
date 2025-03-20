33ac18c257d45ab57a86ffc7e75c65ce3047d1cf
Santiago Hernandez <trollearvpn156@gmail.com>
2025-03-20 15:28:25 -0500

    Se arreglaron errores y ya se puede acceder a la BD

--- Conexiones/ConexionEF3.cs
+++ Conexiones/ConexionEF3.cs
@@ -4,7 +4,7 @@ namespace RetroBusters.Conexiones;
 
 public class ConexionEF3
     {
-        private string string_conexion = "server=SANTY\\SQLEXPRESS;database=RetroBusters;Integrated Security=True;TrustServerCertificate=true;"; //RECORDAR PREGUNTARLE AL PROFESOR POR LA MODIFICACION DE LA INSTANCIA
+        private string string_conexion = "server=SANTY\\SQLEXPRESS;database=RetroBusters;Integrated Security=True;TrustServerCertificate=true;";
         // server=localhost;database=db_instrumentos;uid=sa;pwd=Clas3sPrO2024_!;TrustServerCertificate=true;
         // server=localhost;database=db_instrumentos;Integrated Security=True;TrustServerCertificate=true;
 
@@ -12,15 +12,102 @@ public class ConexionEF3
         {
             var conexion = new Conexion3();
             conexion.StringConnection = this.string_conexion;
+            
+// Obtener y mostrar Almacenes
+var listaAlmacenes = conexion.Almacenes!.ToList();
+foreach (var elementoAlmacen in listaAlmacenes)
+{
+    Console.WriteLine($"Id: {elementoAlmacen.Id_bodega}, " +
+                      $"Ubicación: {elementoAlmacen.Ubicacion_bodega}, " +
+                      $"Capacidad: {elementoAlmacen.Capacidad_bodega}");
+}
 
-            var lista = conexion.Almacenes!.ToList();
+// Obtener y mostrar Miembros
+var listaMiembros = conexion.Miembros!.ToList();
+foreach (var elementoMiembro in listaMiembros)
+{
+    Console.WriteLine($"Id: {elementoMiembro.Id_miembros}, " +
+                      $"Nombre: {elementoMiembro.Nombre}, " +
+                      $"Fecha de Registro: {elementoMiembro.Fecha_registro}, " +
+                      $"Nivel: {elementoMiembro.Nivel}, " +
+                      $"Puntos: {elementoMiembro.Puntos}");
+}
 
-            foreach (var elemento in lista)
-            {
-                Console.WriteLine(elemento.Ubicacion_bodega.ToString() + ", " + 
-                    elemento.Capacidad_bodega.ToString()+ ", "+
-                    elemento.Id_bodega.ToString());
-            }
+// Obtener y mostrar Empleados
+var listaEmpleados = conexion.Empleados!.ToList();
+foreach (var elementoEmpleado in listaEmpleados)
+{
+    Console.WriteLine($"Id: {elementoEmpleado.Id_empleados}, " +
+                      $"Nombre: {elementoEmpleado.Nombre_empleado}, " +
+                      $"Cargo: {elementoEmpleado.Cargo_empleado}, " +
+                      $"Sueldo: {elementoEmpleado.Sueldo}, " +
+                      $"Fecha de Contratación: {elementoEmpleado.Fecha_contratacion}");
+}
+
+// Obtener y mostrar Envios
+var listaEnvios = conexion.Envios!.ToList();
+foreach (var elementoEnvio in listaEnvios)
+{
+    Console.WriteLine($"Id: {elementoEnvio.Id_envios}, " +
+                      $"Estado: {elementoEnvio.Estado}, " +
+                      $"Dirección: {elementoEnvio.Direccion}, " +
+                      $"Transportadora: {elementoEnvio.Transportadora}, " +
+                      $"EmpleadoId: {elementoEnvio.empleados}");
+}
+
+// Obtener y mostrar Reservas
+var listaReservas = conexion.Reservas!.ToList();
+foreach (var elementoReserva in listaReservas)
+{
+    Console.WriteLine($"Id: {elementoReserva.Id_Reserva}, " +
+                      $"Fecha de Reserva: {elementoReserva.Fecha_Reserva}, " +
+                      $"Estado: {elementoReserva.Estado}, " +
+                      $"MiembroId: {elementoReserva.MiembroId}, " +
+                      $"PeliculaId: {elementoReserva.PeliculaId}, " +
+                      $"ConsolaId: {elementoReserva.ConsolaId}, " +
+                      $"EmpleadoId: {elementoReserva.EmpleadoId}");
+}
+
+// Obtener y mostrar Peliculas
+var listaPeliculas = conexion.Peliculas!.ToList();
+foreach (var elementoPelicula in listaPeliculas)
+{
+    Console.WriteLine($"Id: {elementoPelicula.Id_pelicula}, " +
+                      $"Nombre: {elementoPelicula.Nombre_pelicula}, " +
+                      $"Género: {elementoPelicula.Genero_Pelicula}, " +
+                      $"Fecha de Estreno: {elementoPelicula.Fecha_Estreno}, " +
+                      $"Estado: {elementoPelicula.Estado_pelicula}");
+}
+
+// Obtener y mostrar Consolas
+var ListaConsolas = conexion.Consolas!.ToList();
+foreach (var elementoC in ListaConsolas)
+{
+    Console.WriteLine($"Id: {elementoC.Id_consola}, " +
+                      $"Nombre: {elementoC.Tipo_consola}, " +
+                      $"Género: {elementoC.Marca_consola}, " +
+                      $"Fecha de Estreno: {elementoC.Estado_consola}, " +
+                      $"Estado: {elementoC.almacen}");
+}
+
+// Obtener y mostrar Snacks
+var listaSnacks = conexion.Snacks!.ToList();
+foreach (var elementoSnack in listaSnacks)
+{
+    Console.WriteLine($"Id: {elementoSnack.Id_Snack}, " +
+                      $"Nombre: {elementoSnack.Nombre}, " +
+                      $"Precio: {elementoSnack.Precio}, " +
+                      $"Stock: {elementoSnack.Stock}");
+}
+
+// Obtener y mostrar Reservas_Snacks
+var listaReservasSnacks = conexion.Reservas_Snacks!.ToList();
+foreach (var elementoReservaSnack in listaReservasSnacks)
+{
+    Console.WriteLine($"Id: {elementoReservaSnack.Id_Reservas_Snacks}, " +
+                      $"SnackId: {elementoReservaSnack.SnackId}, " +
+                      $"Reserva: {elementoReservaSnack.Reserva}");
+}
         }
 
         /*public void ConexionInsert()
@@ -59,6 +146,7 @@ public class ConexionEF3
         public DbSet<Envios>? Envios { get; set; }
         public DbSet<Reservas>? Reservas { get; set; }
         public DbSet<Peliculas>? Peliculas { get; set; }
+        public DbSet<Consolas>? Consolas { get; set; }
         public DbSet<Snacks>? Snacks { get; set; }
         public DbSet<Reservas_Snacks>? Reservas_Snacks { get; set; }
     }
--- Modelos/ModelosRB.cs
+++ Modelos/ModelosRB.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace RetroBusters.Modelos;
 
 public class Almacenes
 {
+    [Key]
     public int Id_bodega { get; set; }
     public string? Ubicacion_bodega { get; set; }
     public decimal Capacidad_bodega { get; set; }
@@ -10,6 +12,7 @@ public class Almacenes
 
 public class Miembros
 {
+    [Key]
     public int Id_miembros { get; set; }
     public string? Nombre { get; set; }
     public DateTime Fecha_registro { get; set; }
@@ -19,6 +22,7 @@ public class Miembros
 
 public class Empleados
 {
+    [Key]
     public int Id_empleados { get; set; }
     public string? Nombre_empleado { get; set; }
     public string? Cargo_empleado { get; set; }
@@ -28,41 +32,44 @@ public class Empleados
 
 public class Envios
 {
+    [Key]
     public int Id_envios { get; set; }
     public string? Estado { get; set; }
     public string? Direccion { get; set; }
     public string? Transportadora { get; set; }
 
-    [ForeignKey("Empleado")]
-    public int EmpleadoId { get; set; }
-    public Empleados? _empleado { get; set; }
+    [ForeignKey("Empleados")]
+    public int empleados { get; set; }
+    public Empleados? Empleados { get; set; }
 }
 
 public class Reservas
 {
+    [Key]
     public int Id_Reserva { get; set; }
     public DateTime Fecha_Reserva { get; set; }
     public string? Estado { get; set; }
     
-    [ForeignKey("Miembros")]
+    [ForeignKey("Id_miembros")]
     public int MiembroId { get; set; }
-    public Miembros? _Miembro { get; set; }
+    public Miembros? Miembro { get; set; }
     
-    [ForeignKey("Peliculas")]
-    public int PeliculaId { get; set; }
-    public Peliculas? _Pelicula { get; set; }
+    [ForeignKey("Id_pelicula")]
+    public int? PeliculaId { get; set; }
+    public Peliculas? Pelicula { get; set; }
     
-    [ForeignKey("Consolas")]
-    public int ConsolaId { get; set; }
-    public Consolas? _Consola { get; set; }
+    [ForeignKey("Id_consola")]
+    public int? ConsolaId { get; set; }
+    public Consolas? Consola { get; set; }
     
-    [ForeignKey("Empleados")]
+    [ForeignKey("Id_empleados")] 
     public int EmpleadoId { get; set; }
-    public Empleados? _Empleado { get; set; }
+    public Empleados? Empleado { get; set; }
 }
 
 public class Peliculas
 {
+    [Key]
     public int Id_pelicula { get; set; }
     public string? Nombre_pelicula { get; set; }
     public string? Genero_Pelicula { get; set; }
@@ -72,33 +79,36 @@ public class Peliculas
 
 public class Consolas
 {
+    [Key]
     public int Id_consola { get; set; }
     public string? Tipo_consola { get; set; }
     public string? Marca_consola { get; set; }
     public int Estado_consola { get; set; }
     
-    [ForeignKey("Almacen")]
+    [ForeignKey("_almacen")]
     public int almacen { get; set; }
     public Almacenes? _almacen { get; set; }
 }
 
 public class Snacks
 {
+    [Key]
     public int Id_Snack { get; set; }
     public string? Nombre { get; set; }
-    public double Precio { get; set; }
+    public decimal Precio { get; set; }
     public int Stock { get; set; }
 }
 
 public class Reservas_Snacks
 {
+    [Key]
     public int Id_Reservas_Snacks { get; set; }
     
-    [ForeignKey("Snacks")]
+    [ForeignKey("_Snack")]
     public int SnackId { get; set; }
     public Snacks? _Snack { get; set; }
     
-    [ForeignKey("Reservas")]
+    [ForeignKey("_Reserva")]
     public int Reserva { get; set; }
     public Reservas? _Reserva { get; set; }
 }
